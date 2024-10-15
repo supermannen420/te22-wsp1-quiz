7b98aa94d9a2f31a894cd02bb64a1226bb51ebf7
elton <eltram06@gmail.com>
2024-10-15 11:16:16 +0200

    bra

---
+++ routes/quiz.js
@@ -0,0 +1,49 @@
+import express from "express"
+const router = express.Router()
+
+router.get("/", (req, res) => {
+  res.render("quiz.njk", {
+    message: "Quiz"
+    })
+})
+
+const questions = [
+  {
+    id: "q1",
+    text: "Hur många hjul har en bil?",
+    answers: [3, 12, 5, 4],
+    correctAnswer: 4
+  },
+  {
+    id: "q2",
+    text: "Hur många ben har en gnu?",
+    answers: ["två", "sju", "tre", "fyra"],
+    correctAnswer: "fyra",
+  }
+]
+
+router.get("/questions", (req, res) => {
+  res.render("questions.njk", {
+    message: "Frågor",
+    questions
+  })
+})
+
+router.post("/end", (req, res) => {
+  const answers = req.body
+  console.log(answers)  
+  questions.forEach(question => {
+    const answer = answers[question.id]
+    if (answer == question.correctAnswer) {
+      console.log("Du har svarat rätt på fråga : ", question.id)
+    }
+  })
+  res.json(answers)
+})
+// rad 4 , 18, 19 i server.js
+
+export default router
+
+// kom ihåg i server.js
+// import quizRouter from './routes/quiz.js'
+// app.use("/quiz", quizRouter)
--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-import express from 'express'
-import nunjucks from 'nunjucks'
+import express from "express"
+import nunjucks from "nunjucks"
 import morgan from "morgan"
-
-import indexRouter from './routes/index.js'
-import searchRouter from './routes/search.js'
+import bodyParser from "body-parser"
+// npm i body-parser
+import indexRouter from "./routes/index.js"
+import searchRouter from "./routes/search.js"
+import quizRouter from "./routes/quiz.js"
 
 const app = express()
 
-nunjucks.configure('views', {
+nunjucks.configure("views", {
     autoescape: true,
     express: app
 })
-app.use(express.static('public'))
+app.use(express.static("public"))
 app.use(morgan("dev"))
-app.use('/search', searchRouter)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use('/', indexRouter)
+app.use("/", indexRouter)
+app.use("/search", searchRouter)
+app.use("/quiz", quizRouter)
 
 // testa med att surfa localhost:3000/asdfhjk
 app.use((req, res) => {
-//    res.status(404).send('404 - Not found')
-  res.status(404).render('404.njk', {
-      title: '404 - Not found',
+//    res.status(404).send("404 - Not found")
+  res.status(404).render("404.njk", {
+      title: "404 - Not found",
   })
 });
 
